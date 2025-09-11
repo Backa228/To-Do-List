c9881891fda2f3e5084d356ab459a35cc0462f13
Backa228 <andrey.scott01@gmail.com>
2025-09-11 16:38:01 +0300

    lesson 11.09
    Бібліотека Formik

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,41 +1,42 @@
-import { useState } from "react"
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
 
 const FeedbackSchema = Yup.object().shape({
     text: Yup.string().min(5, "Too Short!").max(100, "Too Long").required("Required"),
     priority: Yup.string().oneOf(["High", "Medium", "Low"]).required("Required"),
+    email: Yup.string().min(2, "Too Short!").max(20, "Too Long").required("Required"),
 })
 
 function TaskForm({onAdd}) {
-    const [text, setText] = useState("")
-    const [priority, setPriority] = useState("Medium")
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if (!text) return
-        onAdd(text, priority),
-        setText("")
+    const handleSubmit = (values, actions) => {
+        onAdd(values.text, values.priority),
+        actions.resetForm()
     }
 
     return (
-        <Formik onSubmit={handleSubmit} initialValues={{text: "", priority: "Medium"}} validationSchema={FeedbackSchema}>
-            <Form onSubmit={handleSubmit}>
+        <Formik onSubmit={handleSubmit} initialValues={{text: "", email: "@gmail.com", priority: "Medium"}} validationSchema={FeedbackSchema}>
+            <Form>
             <Field type="text"
                 name="text"
                 placeholder="New task..."
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-            />
-            <Field as="select"
-                value={priority}
-                onChange={(e) => setPriority(e.target.value)}>
+                />
+                <Field type="email"
+                    name="email"
+                    placeholder="email"
+                >
+                </Field>
+                <Field as="select"
+                    name="priority">
                     <option value="High">High</option>
                     <option value="Medium">Medium</option>
                     <option value="Low">Low</option>
                 </Field>
-            <button type="submit">Add task</button>
-        </Form>
+                <button type="submit">Add task</button>
+                <br />
+                <ErrorMessage name='text' component='span' style={{ color: "red", fontSize: "0.8rem" }} />
+                <ErrorMessage name='email' component='span' style={{ color: "red", fontSize: "0.8rem" }} />
+            </Form>
         </Formik>
         
     )
